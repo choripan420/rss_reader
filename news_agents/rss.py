9a225349867a08cc75ac102b17eff78ec8f604b4
choripan420 <68100189+choripan420@users.noreply.github.com>
2023-10-03 05:00:50 +0000

    remove solution

--- news_agents/rss.py
+++ news_agents/rss.py
@@ -3,39 +3,7 @@ import trafilatura
 import pickle
 
 class FeedReader:
-    def __init__(self, url):
-        self.url = url
-        try:
-            feed = feedparser.parse(self.url)
-            # A simple check to see if we got valid feed data
-            if not feed.entries:
-                raise ValueError("Invalid feed data")
-        except:
-            print('Getting RSS failed. Using mocked response instead.')
-            with open('bbc_feed.pkl', 'rb') as file:
-                feed = pickle.load(file)
-
-        self.feed = [
-            {
-                key: entry.get(key, None)
-                for key in ["title", "summary", "link", "published"]
-            }
-            for entry in feed.entries
-        ]
-
-    def read_article(self, index):
-        try:
-            article_url = self.feed[index]["link"]
-            downloaded = trafilatura.fetch_url(article_url)
-            text = trafilatura.extract(downloaded)
-            return {
-                "metadata": self.feed[index],
-                "body": text,
-            }
-        except Exception as e:
-            return {
-                "error": str(e),
-            }
+    # your code goes here
         
 
 reader = FeedReader("http://feeds.bbci.co.uk/news/rss.xml")
